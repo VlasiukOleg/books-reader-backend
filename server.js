4a254ae91be727d6fc89b3cf6c0b421a39217c55
Oleg <ikslprint@gmail.com>
2023-08-01 17:39:07 +0300

    moved DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const DB_HOST = 'mongodb+srv://Oleg:XEBd7NEhKAnkCREp@cluster0.3z6iy0y.mongodb.net/books_reader?retryWrites=true&w=majority'
+// const DB_HOST = 'mongodb+srv://Oleg:XEBd7NEhKAnkCREp@cluster0.3z6iy0y.mongodb.net/books_reader?retryWrites=true&w=majority'
+
+const {DB_HOST} = process.env
 
 mongoose.set('strictQuery', true);
 
@@ -12,5 +14,7 @@ mongoose.connect(DB_HOST).then(app.listen(3000, () => {
   console.log(error.message);
   process.exit(1);
 })
+
+console.log(process.env);
 
 
