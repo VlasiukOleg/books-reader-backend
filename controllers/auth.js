b00bcb55a8f3439c1364af0103ee0f416caacd7f
Oleg <ikslprint@gmail.com>
2023-08-04 15:57:37 +0300

    add auth

---
+++ controllers/auth.js
@@ -0,0 +1,90 @@
+const Joi = require('joi');
+
+const User = require('../models/user')
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken')
+
+
+const {HttpError} = require('../helpers')
+
+// const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {SECRET_KEY} = process.env;
+
+
+
+const registerSchema  = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+})
+
+const loginSchema  = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+})
+
+
+const register = async (req, res,next) => {
+    try {
+        const {error} = registerSchema.validate(req.body)
+        if (error) {
+        throw HttpError(400, error.message);
+        }
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if (user) {
+            throw HttpError(409, 'Email is already use');
+            }
+        
+        const hashPassword = await bcrypt.hash(password,10);
+
+        const newUser = await User.create({...req.body, password: hashPassword });
+        res.status(201).json({
+            name: newUser.name,
+            email: newUser.email,
+        })
+    } catch (error) {
+        next(error);
+    }
+    
+}
+
+const login = async (req,res,next) => {
+    try {
+        const {error} = loginSchema.validate(req.body)
+        if (error) {
+        throw HttpError(400, error.message);
+        }
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            throw HttpError(401, 'Email or password invalid');
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            throw HttpError(401, 'Email or password invalid');
+        }
+        const payload = {
+            id: user._id,
+        }
+        const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'})
+        res.json({
+            token,
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+module.exports = {
+    register,
+    login
+}
---
+++ models/user.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require('mongoose');
+
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        match: emailRegex,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true,
+    }
+}, {versionKey: false, timestamps: true })
+
+
+// !Щоб статус викидав при помилці
+userSchema.post('save', (error, data, next) => {
+    const {name, code} = error;
+    // !409 MongoServerError коли реэструэмось а такий email вже є
+    const status = (name === 'MongoServerError' || code === 11000) ? 409 : 400;
+    error.status =status;
+    next();
+})
+
+
+const User = model('user', userSchema);
+
+
+module.exports = User;
